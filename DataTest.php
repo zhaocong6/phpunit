480a11ff6f3363c4a147c70dbb814d72e350ad22
zhaocong6 <1140253608@qq.com>
2018-09-26 23:08:16 +0800

    .

---
+++ DataTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class DataTest extends TestCase
+{
+
+    /**
+     * @dataProvider additionProvider
+     */
+    public function testAdd($a, $b, $expected)
+    {
+        $this->assertEquals($expected, $a + $b);
+    }
+
+    public function additionProvider()
+    {
+        return [
+            'adding zeros'  => [0, 0, 0],
+            'zero plus one' => [0, 1, 1],
+            'one plus zero' => [1, 0, 1],
+            'one plus one'  => [1, 1, 3]
+        ];
+    }
+}
---
+++ DependencyAndDataProviderComboTest.php
@@ -0,0 +1,28 @@
+<?php
+use PHPUnit\Framework\TestCase;
+class DependencyAndDataProviderComboTest extends TestCase
+{
+    public function provider()
+    {
+        return [['provider1'], ['provider2']];
+    }
+    public function testProducerFirst()
+    {
+        $this->assertTrue(true);
+        return 'first';
+    }
+    public function testProducerSecond()
+    {
+        $this->assertTrue(true);
+        return 'second';
+    }
+    /**
+     * @depends testProducerFirst
+     * @depends testProducerSecond
+     * @dataProvider provider
+     */
+    public function testConsumer(... $params)
+    {
+        $this->assertEquals(['provider1', 'first', 'second'], $params);
+    }
+}
