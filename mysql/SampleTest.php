2802557e32e91d74b7c13edff195715e87ca69d9
zhaocong6 <1140253608@qq.com>
2018-09-28 16:47:20 +0800

    mysql数据库测试

--- mysql/SampleTest.php
+++ mysql/SampleTest.php
@@ -4,6 +4,7 @@ class SampleTest extends GenericTestsDatabaseTestCase
 {
     public function testSomething()
     {
+        //phpunit --bootstrap GenericTestsDatabaseTestCase.php --configuration mysql.xsl SampleTest.php
         $db = $this->getConnection();
         $count = $db->query("select * from test")->fetch();
 
